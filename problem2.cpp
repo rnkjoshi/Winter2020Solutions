1ab01b56439a1bf712818901f05ff9645114746e
RONAK JOSHI <51354961+rnkjoshi@users.noreply.github.com>
2020-05-19 23:45:19 +0530

    Add files via upload

---
+++ problem2.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+#include<string>
+typedef long long int ll;
+
+using namespace std;
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    ll t,n;
+    cin>>t;
+    while(t--){
+        cin>>n;
+        ll prime=3;
+        for(int i=0;prime<n;i++){
+                ll prime sum
+            for(int j=0;j<)
+        }
+    }
+}
+
